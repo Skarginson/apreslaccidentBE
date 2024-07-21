46ec64dd635c1163113849e787e37644a9c34187
Skarginson <eldaraes@gmail.com>
2024-07-21 16:54:11 +0200

    WritingPrompt Model

---
+++ src/models/WritingPrompt.ts
@@ -0,0 +1,16 @@
+// src/models/WritingPrompt.ts
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IWritingPrompt extends Document {
+  card: string;
+  text: string;
+}
+
+const WritingPromptSchema: Schema<IWritingPrompt> = new Schema({
+  card: { type: String, required: true },
+  text: { type: String, required: true }
+});
+
+const WritingPrompt = mongoose.model<IWritingPrompt>('WritingPrompt', WritingPromptSchema);
+
+export default WritingPrompt;
